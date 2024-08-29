ff8adc96ccf5310150a0642339387c8b3902feaf
SingularTeabag <129084658+SingularTeabag@users.noreply.github.com>
2024-08-29 14:07:01 +0000

    Added Files

---
+++ nspire/Blackjack.py
@@ -0,0 +1,8 @@
+from ti_draw import *
+
+while get_key() != ’esc’:
+    set_window( -159, 159, -106, 106)
+    set_color(100, 100, 100)
+    fill_rect(-159, - 106, 318, 212)
+    set_color(255,0,0)
+    fill_circle(0,0,100)
